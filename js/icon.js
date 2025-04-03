77c7d0d264de79ed718a4c61b46826fdf0ea1de2
Bomin Kim <sirubom1203@gmail.com>
2025-04-03 23:30:04 +0900

    🐛Fix: 내용수정

--- js/icon.js
+++ js/icon.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
   // 아이콘 요소와 이모지가 담긴 목록
   const icon = document.getElementById('emojiBtn');
-  const picker = new EmojiButton({ theme: 'auto', position: 'bottom-' });
+  const picker = new EmojiButton({ theme: 'auto', position: 'bottom-start' });
   // 아이콘 요소 클릭시, 이모지 목록 펼침
   icon.addEventListener('click', () => {
     picker.togglePicker(icon);
