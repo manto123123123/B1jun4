892fa01fd224b7fbd0ade550730033629adf8512
YEONSEO <yeonseochoi0717@gmail.com>
2025-04-03 17:28:46 +0900

    🗃️Comment: show.js의 css, html 주석 제거

--- js/show.js
+++ js/show.js
@@ -12,7 +12,7 @@ document.addEventListener('DOMContentLoaded', () => {
     try {
       const response = await fetch(API, {
         headers: {
-          'Content-Type': 'application/json', // title null로 들어가는 것 해결
+          'Content-Type': 'application/json',
           'x-username': USERNAME,
         },
       });
@@ -55,27 +55,6 @@ function showDocuments(doc, depth) {
   // document로 이동시켜주는 a 태그
   const documentLink = document.createElement('a');
   documentLink.href = '#';
-
-  // document 열림, 닫힘 보여주는 화살표
-  /*
-sideBar.css 에 스타일 추가
-.arrow {
-border: solid #c1c1c1;
-border-width: 0 3px 3px 0;
-display: inline-block;
-padding: 3px;
-}
-
-.right {
-transform: rotate(-45deg);
--webkit-transform: rotate(-45deg);
-}
-
-.down {
-transform: rotate(45deg);
--webkit-transform: rotate(45deg);
-}
-  */
   const documentToggle = document.createElement('i');
   documentToggle.classList.add('arrow', 'right'); // 닫힘 상태
   documentLink.appendChild(documentToggle);
@@ -96,22 +75,6 @@ transform: rotate(45deg);
   const addDocumentBtn = document.createElement('button');
   const fileAdd = document.createElement('span');
   fileAdd.textContent = 'add';
-  /*
-posting.html에 
-<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0&icon_names=add"/>
-추가,
-
-sideBar.css에
-.material-symbols-outlined {
-font-variation-settings:
-  'FILL' 0,
-  'wght' 400,
-  'GRAD' 0,
-  'opsz' 24;
-color: #c1c1c1;
-}
-추가
-  */
   fileAdd.classList.add('material-symbols-outlined');
   addDocumentBtn.appendChild(fileAdd);
   documentList.appendChild(addDocumentBtn);
