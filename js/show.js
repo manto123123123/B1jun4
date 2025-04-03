8274006b93f65e0019d5302eba29ed81a658b00d
YEONSEO <yeonseochoi0717@gmail.com>
2025-04-03 11:08:12 +0900

    ♻️Refactor: show.js 변경된 prettier 스타일 적용

--- js/show.js
+++ js/show.js
@@ -4,36 +4,36 @@ const USERNAME = 'b1jun4'
 const showAll = document.getElementById('showAll')
 
 document.addEventListener('DOMContentLoaded', () => {
-    // addDocument는 파일 추가 button 태그 id
-    //   const addDocument = document.getElementById('addDocument')
-    //   addDocument.addEventListener('click', postDocument)
+  // addDocument는 파일 추가 button 태그 id
+  //   const addDocument = document.getElementById('addDocument')
+  //   addDocument.addEventListener('click', postDocument)
 
-    async function getDocuments() {
-        try {
-            const response = await fetch(API, {
-                headers: {
-                    'Content-Type': 'application/json;charset=utf-8', // title null로 들어가는 것 해결
-                    'x-username': USERNAME,
-                },
-            })
+  async function getDocuments() {
+    try {
+      const response = await fetch(API, {
+        headers: {
+          'Content-Type': 'application/json;charset=utf-8', // title null로 들어가는 것 해결
+          'x-username': USERNAME,
+        },
+      })
 
-            if (!response.ok) {
-                throw new Error('Network response was not ok!')
-            }
-            const data = await response.json()
-            console.log('성공: ', data)
+      if (!response.ok) {
+        throw new Error('Network response was not ok!')
+      }
+      const data = await response.json()
+      console.log('성공: ', data)
 
-            for (let doc of data) {
-                const title = doc.title
-                // document 요소
-                const documentList = document.createElement('li')
+      for (let doc of data) {
+        const title = doc.title
+        // document 요소
+        const documentList = document.createElement('li')
 
-                // document로 이동시켜주는 a 태그
-                const documentLink = document.createElement('a')
-                documentLink.href = '#'
+        // document로 이동시켜주는 a 태그
+        const documentLink = document.createElement('a')
+        documentLink.href = '#'
 
-                // document 열림, 닫힘 보여주는 화살표
-                /*
+        // document 열림, 닫힘 보여주는 화살표
+        /*
 sideBar.css 에 스타일 추가
 .arrow {
   border: solid #c1c1c1;
@@ -52,26 +52,26 @@ sideBar.css 에 스타일 추가
   -webkit-transform: rotate(45deg);
 }
         */
-                const documentToggle = document.createElement('i')
-                documentToggle.classList.add('arrow', 'right') // 닫힘 상태
-                documentLink.appendChild(documentToggle)
+        const documentToggle = document.createElement('i')
+        documentToggle.classList.add('arrow', 'right') // 닫힘 상태
+        documentLink.appendChild(documentToggle)
 
-                // document 아이콘
-                const documentIcon = document.createElement('img')
-                documentLink.appendChild(documentIcon)
+        // document 아이콘
+        const documentIcon = document.createElement('img')
+        documentLink.appendChild(documentIcon)
 
-                // document 제목
-                const documentTitle = document.createTextNode(title)
-                documentLink.appendChild(documentTitle)
-                // documentLink.textContent = title
+        // document 제목
+        const documentTitle = document.createTextNode(title)
+        documentLink.appendChild(documentTitle)
+        // documentLink.textContent = title
 
-                // documentList 자식으로 documentLink 추가
-                documentList.appendChild(documentLink)
+        // documentList 자식으로 documentLink 추가
+        documentList.appendChild(documentLink)
 
-                // documentList 자식으로 파일 추가 버튼 추가
-                const fileAdd = document.createElement('span')
-                fileAdd.textContent = 'add'
-                /*
+        // documentList 자식으로 파일 추가 버튼 추가
+        const fileAdd = document.createElement('span')
+        fileAdd.textContent = 'add'
+        /*
 posting.html에 
 <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0&icon_names=add"/>
 추가,
@@ -87,19 +87,19 @@ sideBar.css에
 }
 추가
         */
-                fileAdd.classList.add('material-symbols-outlined')
-                console.log(fileAdd)
-                documentList.appendChild(fileAdd)
+        fileAdd.classList.add('material-symbols-outlined')
+        console.log(fileAdd)
+        documentList.appendChild(fileAdd)
 
-                console.log(documentList)
+        console.log(documentList)
 
-                // ul 태그 자식으로 documentList 추가
-                showAll.appendChild(documentList)
-            }
-        } catch (error) {
-            console.error('실패: ', error)
-        }
+        // ul 태그 자식으로 documentList 추가
+        showAll.appendChild(documentList)
+      }
+    } catch (error) {
+      console.error('실패: ', error)
     }
+  }
 
-    getDocuments()
+  getDocuments()
 })
