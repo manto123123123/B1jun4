493a7b7a49ce1a481e5a41b660eda593b6503020
YEONSEO <yeonseochoi0717@gmail.com>
2025-04-03 16:31:31 +0900

    ✨Feat: 파일 생성 기능 추가

---
+++ js/posting.js
@@ -0,0 +1,32 @@
+const API = 'https://kdt-api.fe.dev-cos.com/documents';
+const USERNAME = 'b1jun4';
+
+document.addEventListener('DOMContentLoaded', () => {
+  // addDocument는 파일 추가 button 태그 id
+  //   const addDocument = document.getElementById('addDocument')
+  //   addDocument.addEventListener('click', postDocument)
+
+  async function postDocument() {
+    try {
+      const response = await fetch(API, {
+        method: 'POST', // POST
+        headers: {
+          'Content-Type': 'application/json',
+          'x-username': USERNAME,
+        },
+        body: JSON.stringify({
+          title: '새 파일',
+          parent: null,
+        }),
+      });
+
+      if (!response.ok) {
+        throw new Error('Network response was not ok!');
+      }
+      const data = await response.json();
+      console.log('성공: ', data);
+    } catch (error) {
+      console.error('실패: ', error);
+    }
+  }
+});
